e013209788ca9a895febbeca59884aba46a3f2a4
itshliakhov <shliakhovdan@gmail.com>
2022-09-23 18:46:14 +0300

    Second commit: 1) in loop changed i value and now loop working only ten times. 2) When loop ends his work, in console.log user can see his last entered value.

--- firstTask.js
+++ firstTask.js
@@ -1,10 +1,10 @@
 /*Напишите функцию, которая запускает цикл. Цикл на каждой итерации предлагает через prompt ввести число, большее 100 (но максимум 10 итераций цикла) . Если посетитель ввёл число меньше ста – попросить ввести ещё раз, и так далее. Если пользователь вводит число больше ста, текст или цикл заканчивает все итерации, то функция выводит в консоль последний ввод пользователя и завершает функцию. */
 function enterInfo() {
-  for (let i = 0; i <= 10; i++) {
-    const promptValue = prompt("Enter your number more than 100");
+  for (let i = 1; i <= 10; i++) {
+    let promptValue = prompt("Enter your number more than 100");
     if (promptValue <= 100) {
       if (i === 10) {
-        console.log("Loop worked 10 times. Reload and try again");
+        console.log("Last entered value was:", promptValue);
         break;
       }
       alert("Enter number more than 100!");
